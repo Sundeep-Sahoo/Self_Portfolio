1c81d42939fd441e7a3e962df57e78b5f17effb6
Sundeep-Sahoo <omsundeepsahoo@gmail.com>
2025-07-08 17:09:19 +0530

    added the img

--- portfolio/src/components/Banner.js
+++ portfolio/src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/prof.jpg";
+import headerImg from "../assets/img/prof.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
